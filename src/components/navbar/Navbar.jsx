bf794966353fe8a3c7b2df4fc1a295c7ba291749
carson <carson_97@live.ca>
2023-05-16 09:29:26 -0700

    commit
    first upload
    
    -just navbar

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import logo from '../../assets/logo.png' // Path to your logo image
+import './navbar.css'
+
+class Navbar extends React.Component {
+  render() {
+    return (
+      <nav className='navbar'>
+        <img src={logo} alt='Logo' className='navbar-logo' />
+        <ul className='navbar-nav'>
+          <li className='nav-item'>
+            <a href='/' className='nav-link'>
+              Home
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a href='/about' className='nav-link'>
+              About
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a href='/services' className='nav-link'>
+              Services
+            </a>
+          </li>
+          <li className='nav-item'>
+            <a href='/contact' className='nav-link'>
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+    )
+  }
+}
+
+export default Navbar
