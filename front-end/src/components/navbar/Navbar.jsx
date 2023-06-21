796a098e15204a2274d585c0ad0d0a7bb2a1ccd8
carson <carson_97@live.ca>
2023-06-21 07:30:20 -0700

    Navbar underline
    -crisp

--- front-end/src/components/navbar/Navbar.jsx
+++ front-end/src/components/navbar/Navbar.jsx
@@ -1,43 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 import { Link } from 'react-router-dom'
 
-class Navbar extends React.Component {
-  render() {
-    return (
-      <nav className='navbar'>
-        <ul className='navbar-nav'>
-          <li className='nav-item'>
-            <a href='/' className='nav-link'>
-              Home
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a href='#about' className='nav-link'>
-              About
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a href='/services' className='nav-link'>
-              Services
-            </a>
-          </li>
-          <li className='nav-item'>
-            <a href='/contact' className='nav-link'>
-              Contact
-            </a>
-          </li>
+const Navbar = () => {
+  const [activeOption, setActiveOption] = useState('')
 
-          <li className='nav-item'>
-            <a href='/book' className='nav-link'>
-              BOOK <br />
-              SPOTLESS
-            </a>
-          </li>
-        </ul>
-      </nav>
-    )
+  const handleOptionClick = (option) => {
+    setActiveOption(option)
   }
+
+  const getNavOptionClass = (option) => {
+    return option === activeOption ? 'nav-link active' : 'nav-link'
+  }
+
+  return (
+    <nav className='navbar'>
+      <ul className='navbar-nav'>
+        <li className='nav-item'>
+          <Link
+            to='/'
+            className={getNavOptionClass('/')}
+            onClick={() => handleOptionClick('/')}
+          >
+            Home
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link
+            to='/about'
+            className={getNavOptionClass('/about')}
+            onClick={() => handleOptionClick('/about')}
+          >
+            About
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link
+            to='/services'
+            className={getNavOptionClass('/services')}
+            onClick={() => handleOptionClick('/services')}
+          >
+            Services
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link
+            to='/contact'
+            className={getNavOptionClass('/contact')}
+            onClick={() => handleOptionClick('/contact')}
+          >
+            Contact
+          </Link>
+        </li>
+        <li className='nav-item'>
+          <Link
+            to='/book'
+            className={getNavOptionClass('/book')}
+            onClick={() => handleOptionClick('/book')}
+          >
+            BOOK <br />
+            SPOTLESS
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  )
 }
 
 export default Navbar
